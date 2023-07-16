d36604c9769bb13fdd76599e4d620e34f37a3589
Fidelis <yonlifidelis2@gmail.com>
2023-07-16 17:09:28 +0100

    Debuggage pour le server

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'elderco',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    render(request, 'home.html', {})
-
+    return render(request, 'home.html', {})
