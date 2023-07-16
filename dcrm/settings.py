69da2286b83dc238fa3ba73a6fb1185c14b53cda
Fidelis <yonlifidelis2@gmail.com>
2023-07-16 16:29:53 +0100

    Corrections

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'elderco',
-        'USER': 'root',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- website/views.py
+++ website/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
 def home(request):
     render(request, 'home.html', {})
-=======
-# Create your views here.
->>>>>>> 1e76b9a7557f34dcfebf42a6d7ca6e6776639096
+
